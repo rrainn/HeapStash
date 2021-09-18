749f9ca24b08bb18b9bdf9dcd5dc37361d07badb
Charlie Fish <fishcharlie@me.com>
2021-09-17 20:23:50 -0600

    Updating Redis plugin

--- lib/plugin/Redis.ts
+++ lib/plugin/Redis.ts
@@ -8,68 +8,37 @@ interface RedisPluginSettings {
 export = (settings: RedisPluginSettings): Plugin => {
 	const redis = new Plugin();
 
-	redis.tasks.get = (id: string): Promise<any> => {
-		return new Promise<any>((resolve, reject) => {
-			settings.client.get(id, (err, result) => {
-				if (err) {
-					reject(err);
-				} else if (result) {
-					try {
-						resolve(JSON.parse(result));
-					} catch (e) {
-						resolve(result);
-					}
-				} else {
-					reject();
-				}
-			});
-		});
-	};
-	redis.tasks.put = (id: string, data: any): Promise<void> => {
-		return new Promise<void>((resolve, reject) => {
-			const callback = (err) => {
-				if (err) {
-					reject(err);
-				} else {
-					resolve();
-				}
-			};
-
-			let dataStr: string;
-			if (typeof data === "object") {
-				dataStr = JSON.stringify(data);
-			} else {
-				dataStr = data;
+	redis.tasks.get = async (id: string): Promise<any> => {
+		const result = await settings.client.get(id);
+		if (result) {
+			try {
+				return JSON.parse(result);
+			} catch (e) {
+				return result;
 			}
+		} else {
+			throw new Error();
+		}
+	};
+	redis.tasks.put = async (id: string, data: any): Promise<void> => {
+		let dataStr: string;
+		if (typeof data === "object") {
+			dataStr = JSON.stringify(data);
+		} else {
+			dataStr = data;
+		}
 
-			if (data.ttl) {
-				settings.client.set(id, dataStr, "EX", Math.round((Date.now() - data.ttl) / 1000), callback);
-			} else {
-				settings.client.set(id, dataStr, callback);
-			}
-		});
+		if (data.ttl) {
+			await settings.client.set(id, dataStr, "EX", Math.round((Date.now() - data.ttl) / 1000));
+		} else {
+			await settings.client.set(id, dataStr);
+		}
 	};
-	redis.tasks.remove = (id: string): Promise<void> => {
-		return new Promise<void>((resolve, reject) => {
-			settings.client.del(id, (err) => {
-				if (err) {
-					reject(err);
-				} else {
-					resolve();
-				}
-			});
-		});
+	redis.tasks.remove = async (id: string): Promise<void> => {
+		await settings.client.del(id);
 	};
-	redis.tasks.clear = (): Promise<void> => {
-		return new Promise<void>((resolve, reject) => {
-			settings.client.flushdb((err) => {
-				if (err) {
-					reject(err);
-				} else {
-					resolve();
-				}
-			});
-		});
+	redis.tasks.clear = async (): Promise<void> => {
+		await settings.client.flushdb();
 	};
 
 	return redis;
