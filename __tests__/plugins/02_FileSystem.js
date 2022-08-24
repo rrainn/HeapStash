2f0033b80ded9a827da6a4f2b65dc77c5af52706
Charlie Fish <fishcharlie@me.com>
2022-08-24 17:20:41 -0600

    Trying to fix test

--- __tests__/plugins/02_FileSystem.js
+++ __tests__/plugins/02_FileSystem.js
@@ -72,7 +72,7 @@ describe("FileSystem", () => {
 		});
 
 		it("Should put item in file system cache with different ttl", async () => {
-			await cache.put("id", {"myitem": "Hello World", "pluginTTL": 500});
+			await cache.put("id", {"myitem": "Hello World"}, {"pluginTTL": 500});
 
 			return new Promise((resolve, reject) => fs.readFile(path.join(__dirname, "tmp", "id"), "utf8", (err, data) => {
 				if (err) {
